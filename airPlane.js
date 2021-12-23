78334b84acd2fe5ce62de6bd0c180446613ec405
Gayane25 <gayabennu@gmail.com>
2021-12-23 23:15:11 +0400

    feat: solved with proto

--- airPlane.js
+++ airPlane.js
@@ -3,3 +3,29 @@
 Give airplanes the ability to .takeOff() and .land():
 If a plane takes off, its isFlying property is set to true.
 If a plane lands, its isFlying property is set to false.*/
+
+const AirPlanObj = {
+  takeOf() {
+    this.isFlying = true;
+  },
+  lands() {
+    this.isFlying = false;
+  },
+};
+
+const aeroflot = {
+  plane: 'Boeing',
+  isFlying: false,
+  __proto__: AirPlanObj,
+};
+
+const airFrance = {
+  plane: 'GreenPeace',
+  __proto__: AirPlanObj,
+  isFlying: false,
+};
+
+aeroflot.lands();
+console.log(aeroflot.isFlying);
+airFrance.takeOf();
+console.log(airFrance.isFlying);
